75f062fb0fa03739d155470c475ea9b71426e1d2
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-10-28 14:52:30 +0430

    Adds my_enumerable.rb module and uses the methods in the my-list class

---
+++ my_enumerable.rb
@@ -0,0 +1,18 @@
+module MyEnumerable
+  def all?
+    each { |e| return false unless yield(e) }
+    true
+  end
+
+  def any?
+    each { |e| return true if yield(e) }
+    false
+  end
+
+  def filter
+    arr = []
+    each { |e| arr.push(e) if yield(e) }
+    arr
+  end
+
+end
